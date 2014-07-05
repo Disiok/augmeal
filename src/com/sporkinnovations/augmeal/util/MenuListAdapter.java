01c5e843368bfe44876a065b8fbefea9107b6a41
Disiok <simon_suo@hotmail.com>
2014-07-05 15:12:10 -0400

    Dependecies

---
+++ src/com/sporkinnovations/augmeal/util/MenuListAdapter.java
@@ -0,0 +1,43 @@
+package com.sporkinnovations.augmeal.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.sporkinnovations.augmeal.InfoActivity;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MenuListAdapter extends BaseAdapter{
+
+	public MenuListAdapter(InfoActivity infoActivity,
+			ArrayList<HashMap<String, String>> detailList) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
